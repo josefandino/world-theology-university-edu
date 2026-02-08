5d0690953b99d0304a7de36954cf22f0f0860ee4
José Fandiño <josefandinod@gmail.com>
2026-02-08 18:21:43 -0500

    add banner

--- src/app/certificates/allied-health-instructor/en-allied-health-instructor/en-allied-health-instructor.component.ts
+++ src/app/certificates/allied-health-instructor/en-allied-health-instructor/en-allied-health-instructor.component.ts
@@ -8,5 +8,5 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EnAlliedHealthInstructorComponent {
-  public banner = '';
+  public banner = 'public/assets/certificates/allied-health-instructor-en.webp';
 }
